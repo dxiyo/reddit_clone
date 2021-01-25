23f81607f3a304b008991cf63b55edc7c3cc01fe
Abdullah Al-Qadmi <qadm4u@gmail.com>
2021-01-25 07:14:41 +0300

    created post component

---
+++ resources/views/components/posts.blade.php
@@ -0,0 +1,20 @@
+<div class="bg-white h-24 border border-gray-300 hover:border-gray-500 rounded-lg flex">
+    <div class="w-10 bg-gray-100 flex flex-col items-center p-2">
+        <a href="#"><i class="fas fa-arrow-up hover:bg-gray-200 p-1 hover:text-red-600"></i></a>
+        <span class="font-medium">000</span>
+        <a href="#"><i class="fas fa-arrow-down hover:bg-gray-200 p-1 hover:text-blue-600"></i></a>
+    </div>
+    <div class="flex flex-col p-2">
+        <span class="text-gray-500 text-xs">
+            Posted by <span class="hover:underline">u/pyo</span> 
+            <span class="hover:underline">22 hours ago</span> 
+        </span>
+        <h3 class="font-bold text-xl mt-2">Post Title</h3>
+        <div>
+            <span class="p-1 mt-2 hover:bg-gray-200 text-gray-500 text-xs font-bold"><i class="fas fa-comment-alt"></i> 50 Comments</span>
+            <span class="p-1 mt-2 hover:bg-gray-200 text-gray-500 text-xs font-bold"><i class="fas fa-share"></i> Share</span>
+            <span class="p-1 mt-2 hover:bg-gray-200 text-gray-500 text-xs font-bold"><i class="fas fa-bookmark"></i> Save</span>
+            <span class="p-1 mt-2 hover:bg-gray-200 text-gray-500 text-xs font-bold"><i class="fas fa-ellipsis-h"></i></span>
+        </div>
+    </div>
+</div>
