b5d6341e17d82e5389d4aa57ed2f27b246581cab
Abdullah Al-Qadmi <qadm4u@gmail.com>
2021-02-02 02:53:32 +0300

    upvotes appear correctly in the subreddit view

--- resources/views/subreddit.blade.php
+++ resources/views/subreddit.blade.php
@@ -2,6 +2,8 @@
 
  @section('content')
  @include('posts', [
-    'posts' => $subreddit->posts
+    'posts' => $subreddit->posts->map(function($post) {
+        return \App\Models\Post::withUpvotes()->where(['id' => $post->id])->get();
+    })->flatten() // getting the upvotes as part of the ->posts relationship. so i have to map on each of the posts and then flatten it to get a clean collection of the subreddits posts with upvotes
 ])
  @endsection
